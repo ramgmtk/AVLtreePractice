7995f4f7503175df8b006201f188e35dff631d4d
ramgmtk <ramgmtk@gmail.com>
2022-10-10 21:14:01 -0700

    Finished avltree insert

--- src/structure.h
+++ src/structure.h
@@ -92,33 +92,27 @@ typename tree<T>::tree_node* tree<T>::recurr_insert(T data, tree_node* curr) {
     if (!curr) {
         return new tree_node(data);
     } else {
-        int right_height, left_height = 0;
+        //insert
         if (data > curr->data) {
             curr->right = this->recurr_insert(data, curr->right);
-            right_height = curr->right->height;
-            if (curr->left) left_height = curr->left->height;
-            if (right_height - left_height > 1)  {
-                if (/*determine ror or rol*/) curr = this->left_rotate(curr);//right on right, rotate left
-                else {
-                    curr->right = this->right_rotate(curr->right);
-                    curr = this->left_rotate(curr);
-                    //right left, rotate right then left.
-                }
-            } else curr->height = right_height > left_height ? right_height + 1 : left_height + 1;
         } else {
-            curr->left = this->recurr_insert(data, curr->left);
-            left_height = curr->left->height;
-            if (curr->right) right_height = curr->right->height;
-            if (left_height - right_height > 1) {
-                if (/*determine lol or lor*/)curr = this->right_rotate(curr); //left on left, rotate right
-                else {
-                    curr->left = this->left_rotate(curr->left);
-                    curr = this->right_rotate(curr);
-                    //left right, rotate left then right
-                }
-            } else curr->height = right_height > left_height ? right_height + 1 : left_height + 1;         
+            curr->left = this->recurr_insert(data, curr->left);         
         }
-        //at some point update the height
+        //update height
+        int right_height = curr->right ? curr->right->height : 0;
+        int left_height = curr->left ? curr->left->height : 0;
+        curr->height = right_height > left_height ? right_height + 1 : left_height + 1;
+        //balacne
+        int balance = right_height - left_height;
+        if (balance > 1 && data > curr->right->data) curr = this->left_rotate(curr); //ror
+        else if (balance < -1 && data <= curr->left->data) curr = this->right_rotate(curr);//lol
+        else if (balance > 1 && data <= curr->right->data) { //rol
+            curr->right = this->right_rotate(curr->right);
+            curr = this->left_rotate(curr);
+        } else if (balance < -1 && data > curr->left->data) { //lor
+            curr->left = this->left_rotate(curr->left);
+            curr = this->right_rotate(curr);
+        }
         return curr;
     }
 }
@@ -134,7 +128,8 @@ typename tree<T>::tree_node* tree<T>::right_rotate(tree_node* root) {
     int rl_height = root->left ? root->left->height : 0;
     int rr_height = root->right ? root->right->height : 0;
     root->height = rl_height > rr_height ? rl_height + 1 : rr_height + 1;
-    left->height = left->left->height ? left->right->height : left->left->height + 1
+    left->height = left->left && left->right->height <= left->left->height ? 
+        left->left->height + 1 : left->right->height + 1;
 
     return left;
 }
@@ -150,7 +145,8 @@ typename tree<T>::tree_node* tree<T>::left_rotate(tree_node* root) {
     int rl_height = root->left ? root->left->height : 0;
     int rr_height = root->right ? root->right->height : 0;
     root->height = rl_height > rr_height ? rl_height + 1 : rr_height + 1;
-    right->height = right->right->height > right->left->height ? right->right + 1 : right->left + 1;
+    right->height = right->right && right->right->height > right->left->height ? 
+        right->right->height + 1 : right->left->height + 1;
 
     return right;
 }
